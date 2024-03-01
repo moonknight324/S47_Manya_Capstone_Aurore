527f06bfeb97798c9d689e8383019ffec6ff9e9f
Manya324 <manya.j@kalvium.community>
2024-03-01 16:39:56 +0530

    Client side created

--- server/server.js
+++ server/server.js
@@ -2,7 +2,15 @@ const spaceHistory = require("./historyData.json")
 const express = require("express");
 const app = express();
 app.get("/history", (req, res) => {
+    try {
+        if (!spaceHistory) {
+            throw new Error("No history found.");
+        }
     res.send(spaceHistory.history);
+    } catch (error) {
+        console.error("Error : ",error.message);
+        res.status(500).send("Internal Server Error");
+    }
 });
 
 app.listen(5000, () => {
