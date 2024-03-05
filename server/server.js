8a61e131b89effbcb75dda7852325adc66647c49
Manya324 <manya.j@kalvium.community>
2024-03-05 15:57:28 +0530

    db schema

--- server/server.js
+++ server/server.js
@@ -1,17 +1,31 @@
-const spaceHistory = require("./historyData.json")
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
-app.get("/history", (req, res) => {
-    try {
-        if (!spaceHistory) {
-            throw new Error("No history found.");
-        }
-    res.send(spaceHistory.history);
-    } catch (error) {
-        console.error("Error : ",error.message);
-        res.status(500).send("Internal Server Error");
-    }
-});
+app.use(express.json());
+
+const MONGO_URI = process.env.MONGOOSE_URI
+
+mongoose.connect(MONGO_URI, { dbName: "usersData" }).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+})
+
+    
+
+// app.get("/history", (req, res) => {
+//     try {
+//         if (!spaceHistory) {
+//             throw new Error("No history found.");
+//         }
+//     res.send(spaceHistory.history);
+//     } catch (error) {
+//         console.error("Error : ",error.message);
+//         res.status(500).send("Internal Server Error");
+//     }
+// });
 
 app.listen(5000, () => {
     console.log("🚀 server running on port 5000");
