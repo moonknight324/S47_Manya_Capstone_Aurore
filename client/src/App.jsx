f23e209816d7129a0b21a2691dc5e4b259be6bcd
Manya324 <manya.j@kalvium.community>
2024-03-11 15:12:29 +0530

    Fetched and displayed data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const apiKey = import.meta.env.VITE_API_KEY;
+  const apiUrl =
+    "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=" +
+    apiKey;
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(apiUrl);
+
+        if (response.status === 200) {
+          setPhotos(response.data.photos);
+          console.log(response.data.photos);
+        } else {
+          console.error("Error: Unable to fetch data from the API.");
+        }
+      } catch (error) {
+        console.error("Error:", error.message);
+      }
+    }
+
+    fetchData();
+  }, [apiUrl]);
 
   return (
     <>
       <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Photos by Curiosity Rover</h1>
+        <ul>
+          {photos.map((photo, index) => (
+            <li key={index}>
+              <p>Camera Name: {photo.camera.full_name}</p>
+              <p>Earth Date: {photo.earth_date}</p>
+              <img src={photo.img_src}></img>
+            </li>
+          ))}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
