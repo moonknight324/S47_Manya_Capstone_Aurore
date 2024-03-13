c2f55a5e67addc94190178827727637820ae350c
Manya324 <manya.j@kalvium.community>
2024-03-13 15:51:10 +0530

    Created login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-// import MarsRover from './FetchedData/MarsRover'
 import LandingPage from './Pages/LandingPage'
+import Login from './Pages/Login'
 import {Routes,Route} from 'react-router-dom'
 
 function App() {
@@ -8,6 +8,8 @@ function App() {
     <>
    <Routes>
     <Route path='/' element={<LandingPage/>}/>
+    <Route path='/login' element={<Login/>}></Route>
+
    </Routes>
     </>
   )
