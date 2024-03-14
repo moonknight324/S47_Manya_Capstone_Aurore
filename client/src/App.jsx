4c931b3563118372a5b8ecf3ddfa9a0784a14fd0
Manya324 <manya.j@kalvium.community>
2024-03-14 15:51:29 +0530

    Created Sign Up page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import LandingPage from './Pages/LandingPage'
 import Login from './Pages/Login'
+import SignUp from './Pages/SignUp'
 import {Routes,Route} from 'react-router-dom'
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
    <Routes>
     <Route path='/' element={<LandingPage/>}/>
     <Route path='/login' element={<Login/>}></Route>
-
+    <Route path='/signup' element={<SignUp/>}></Route>
    </Routes>
     </>
   )
--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,44 +1,43 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import '../Styles/Login.css'; 
+import { IoMail } from "react-icons/io5";
+import { Link } from 'react-router-dom';
 
-function LoginForm() {
+function SignUpForm() {
 
   return (
     <div className="loginPage">
     <div className="wrapper">
       <form>
-        <h1>Login</h1>
+        <h1>Sign Up</h1>
         <div className="input-box">
           <input
             type="text"
             placeholder="Username"
-         
             required
           />
-          <i className='bx bxs-user'></i>
+        <i className='bx bxs-user'></i>
         </div>
+        <div className="input-box">
+          <input
+            type="email"
+            placeholder="Email"
+            required
+          />
+          <i><IoMail /></i>
+        </div>
+
         <div className="input-box">
           <input
             type="password"
             placeholder="Password"
-          
             required
           />
           <i className='bx bxs-lock-alt'></i>
         </div>
-        <div className="remember-forgot">
-          <label>
-            <input
-              type="checkbox"
-            />
-            Remember Me
-          </label>
-          <a href="#">Forgot Password</a>
-        </div>
-        <button type="submit" className="btn">Login</button>
+        <button type="submit" className="btn">Sign Up</button>
         <div className="register-link">
-          <p>Don't have an account? <Link to={"/signup"}>Register</Link></p>
+          <p>Already have an account? <Link to={"/login"}>Login</Link></p>
         </div>
       </form>
     </div>
@@ -46,4 +45,4 @@ function LoginForm() {
   );
 }
 
-export default LoginForm;
+export default SignUpForm;
